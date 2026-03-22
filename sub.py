cf78bb5decb38f9eb37b6b3965dd4000aced2647
c0a250578c <c0a250578c@edu.teu.ac.jp>
2026-03-22 16:52:54 +0900

    Fix CORS: add expose_headers and max_age for GitHub Pages

--- sub.py
+++ sub.py
@@ -54,9 +54,11 @@ app = FastAPI(title="Amlhere API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=3600,
 )
 
 # ---------------------------------------------------------------------------
